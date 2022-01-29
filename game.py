2343582a8cc35b775863103fcb243ec234932b90
llinauer <lukaslinauer1@gmail.com>
2022-01-29 16:51:43 +0100

    Add 1 sec delay between players and agents turn

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ and computer vs. computer
 
 """
 
+import time
 import tkinter
 import numpy as np
 from state import State, get_hash
@@ -43,9 +44,10 @@ class Game:
         # initialize Fields
         for i in range(BOARD_ROWS):
             for j in range(BOARD_COLS):
-                self.game_fields[i][j] = tkinter.Button(text='', font=('normal', 60, 'normal'),
-                                                        width=5, height=3,
-                                                        command=lambda r=i, c=j: self.field_clicked(r, c))
+                self.game_fields[i][j] = tkinter.Button(
+                        text='', font=('normal', 60, 'normal'),
+                        width=5, height=3,
+                        command=lambda r=i, c=j: self.field_clicked(r, c))
                 self.game_fields[i][j].grid(row=i, column=j)
 
         # announce Label for showing whose turn it is, who won, etc..
@@ -130,7 +132,6 @@ class Game:
             self.game_fields[row][col].config(text=self.player2.name)
             self.announce_label.config(text="It's " + self.player1.name + "s turn")
         self.state.update_state((row, col))
-
         # check for win/draw
         win_int = self.state.check_win()
 
@@ -142,6 +143,8 @@ class Game:
             self.announce_label.config(text="Draw!")
         else:
             # computer makes turn
+            self.window.update() # update the window
+            time.sleep(1) # give the agent time to think
             self.computer_turn()
 
     def computer_turn(self):
@@ -165,7 +168,6 @@ class Game:
             self.announce_label.config(text="It's " + self.player1.name + "s turn")
 
         self.state.update_state(action)
-
         # check for win/draw
         win_int = self.state.check_win()
 
